b95058e5a434ed43f8b2eeb18afe68bba380a9e0
Kyan Kersten <KJM.kersten@student.han.nl>
2024-10-21 10:33:55 +0200

    Level 1 Evaluator
    - Implementatie van Level 1 evaluator

--- startcode/src/main/java/nl/han/ica/icss/transforms/Evaluator.java
+++ startcode/src/main/java/nl/han/ica/icss/transforms/Evaluator.java
@@ -1,9 +1,11 @@
 package nl.han.ica.icss.transforms;
 
+import nl.han.ica.datastructures.HANLinkedList;
 import nl.han.ica.datastructures.IHANLinkedList;
 import nl.han.ica.icss.ast.*;
 import org.checkerframework.checker.units.qual.A;
 
+import javax.swing.text.Style;
 import java.util.HashMap;
 
 public class Evaluator implements Transform {
@@ -11,17 +13,28 @@ public class Evaluator implements Transform {
     private IHANLinkedList<HashMap<String, Literal>> variableValues;
 
     public Evaluator() {
-        //variableValues = new HANLinkedList<>();
+        variableValues = new HANLinkedList<>();
+        variableValues.addFirst(new HashMap<>());
     }
 
     @Override
     public void apply(AST ast) {
-        //variableValues = new HANLinkedList<>();
-        applyStyleSheet((Stylesheet)ast.root);
+        applyStyleSheet(ast.root);
     }
 
     private void applyStyleSheet(Stylesheet node) {
-            applyStyleRule((Stylerule) node.getChildren().get(0));
+        for (ASTNode child : node.getChildren()) {
+            if (child instanceof Stylerule) {
+                applyStyleRule((Stylerule) child);
+            } else if (child instanceof VariableAssignment) {
+                applyVariableAssignment((VariableAssignment) child);
+            }
+        }
+    }
+
+    private void applyVariableAssignment(VariableAssignment node) {
+        Literal value = (Literal) evalExpression(node.expression);
+        variableValues.getFirst().put(node.name.name, value);
     }
 
     private void applyStyleRule(Stylerule node) {
@@ -37,6 +50,26 @@ public class Evaluator implements Transform {
     }
 
     private Expression evalExpression(Expression expression) {
+        if (expression instanceof VariableReference){
+            VariableReference ref = (VariableReference) expression;
+            Literal value = lookupVariableValue(ref.name);
+
+            if (value != null){
+                return value;
+            } else {
+                expression.setError("Variable " + ref.name + " not defined.");
+            }
+        }
         return expression;
+    }
+
+    private Literal lookupVariableValue(String variableName) {
+        for (int i = 0; i < variableValues.getSize(); i++) {
+            HashMap<String, Literal> currentScope = variableValues.get(i);
+            if (currentScope.containsKey(variableName)) {
+                return currentScope.get(variableName);
+            }
+        }
+        return null;
     }
 }
